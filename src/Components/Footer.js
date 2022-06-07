03ab123244bbbc782fb1c654fd0dec4f11ad9646
Misterless <healwinds@gmail.com>
2022-06-07 16:22:04 +0900

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <>
+            <h3>Footer 페이지</h3>
+        </>
+    );
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+    return(
+        <>
+        <h2>
+            ▤  Mainheading login signin
+        </h2>
+        </>
+    );
+};
+
+export default Header;
